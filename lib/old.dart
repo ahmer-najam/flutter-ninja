0007eab928d0206a2f7ab08bc9ab2b56b21aa9ee
Ahmer Najam <ahmer.najam@hotmail.com>
2020-03-02 07:26:24 +0500

    InitCommit

---
+++ lib/old.dart
@@ -0,0 +1,64 @@
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text("My First App.."),
+//           centerTitle: true,
+//           backgroundColor: Colors.red[400],
+//         ),
+//         body: Row(
+//             mainAxisAlignment: MainAxisAlignment.end,
+//             crossAxisAlignment: CrossAxisAlignment.end,
+//             children: <Widget>[
+//               Expanded(
+//                 flex: 3,
+//                 child: Container(
+//                   padding: EdgeInsets.all(30),
+//                   color: Colors.blueGrey[600],
+//                   child: Text("1"),
+//                 ),
+//               ),
+//               Expanded(
+//                 flex: 2,
+//                 child: Container(
+//                   padding: EdgeInsets.all(30),
+//                   color: Colors.amber[600],
+//                   child: Text("2"),
+//                 ),
+//               ),
+//               Expanded(
+//                 flex: 1,
+//                 child: Container(
+//                   padding: EdgeInsets.all(30),
+//                   color: Colors.black12,
+//                   child: Text("3"),
+//                 ),
+//               ),
+//             ])
+//         // Center(
+//         //     child: RaisedButton.icon(
+//         //   onPressed: () {},
+//         //   icon: Icon(Icons.mail),
+//         //   label: Text(
+//         //     "Mail Me..",
+//         //     style: TextStyle(fontFamily: "Oxanium", fontSize: 20),
+//         //   ),
+//         //   color: Colors.amber,
+//         // )
+//         // child: Image.asset("assets/Mobile1.jpg"),
+
+//         // Text(
+//         //   'Hello World!!!',
+//         //   style: TextStyle(
+//         //       color: Colors.deepOrange, fontFamily: 'Oxanium', fontSize: 30),
+//         // ),
+//         // ),
+//         // floatingActionButton: FloatingActionButton(
+//         //   child: Text("Click"),
+//         //   onPressed: () {},
+//         //   backgroundColor: Colors.red[400],
+//         // ),
+//         );
+//   }
+// }
