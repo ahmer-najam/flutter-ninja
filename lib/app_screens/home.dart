d60d0edd2560ce7757a587b5fa4428fd1194ed5b
Ahmer Najam <ahmer.najam@hotmail.com>
2020-03-09 07:19:10 +0500

    2020.03.09.001

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -5,17 +5,65 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        padding: EdgeInsets.only(top: 40.0, left: 10.0),
         alignment: Alignment.center,
         color: Colors.deepPurple,
-        // height: 100.0,
-        // width: 300.0,
-        child: Text(
-          'Flight',
-          style: TextStyle(
-              fontFamily: 'Segoe UI',
-              fontSize: 40.0,
-              fontWeight: FontWeight.w200,
-              color: Colors.white),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                  'Air Blue',
+                  style: TextStyle(
+                      fontFamily: 'Raleway',
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                      decoration: TextDecoration.none),
+                )),
+                Expanded(
+                  child: Text(
+                    'From Karachi to Islamabad',
+                    style: TextStyle(
+                        fontFamily: 'Raleway',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                        decoration: TextDecoration.none),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                  'Shaheen Air',
+                  style: TextStyle(
+                      fontFamily: 'Raleway',
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                      decoration: TextDecoration.none),
+                )),
+                Expanded(
+                  child: Text(
+                    'From Lahore to Karachi',
+                    style: TextStyle(
+                        fontFamily: 'Raleway',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                        decoration: TextDecoration.none),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
